a9e2434b8db7c53805b47715739ff9933d34431a
Hayley Elahee <71637025+HayleyBunny@users.noreply.github.com>
2021-02-21 00:58:44 +0000

    Add files via upload

---
+++ Pet.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pet_Simulator
+{
+    class Pet
+    {
+        public int HappinessMeter;
+        private string name;
+        public int NumberOfMeals;
+
+        public Pet(string mPetName)
+        {
+            name = mPetName;
+        }
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+        }
+        public virtual void GiveFood()
+        {
+            NumberOfMeals++;
+            Console.WriteLine("Congratualtions! " + Name + " ate some food! YUMMMY ");
+        }
+    }
+
+    class Mammal : Pet
+    {
+        public int TimesWatered;
+        public int TimesBathed;
+        public Mammal(string name) : base(name)
+        {
+        }
+        public void PetHead()
+        {
+            Console.WriteLine("You pet " + Name + "'s head! Cute!!");
+            HappinessMeter++;
+        }
+        public void GiveWater()
+        {
+            Console.WriteLine("Gulp Gulp! " + Name + " has drunk delicous water!");
+            TimesWatered++;
+        }
+        public virtual void GiveBath()
+        {
+            Console.WriteLine("Splish Splash, " + Name + " took a bath!!");
+            TimesBathed++;
+        }
+        public void BrushFur()
+        {
+            Console.WriteLine(Name + "'s fur is nice and soft! So pretty!");
+            HappinessMeter++;
+        }
+    }
+
+    class Cat : Mammal
+    {
+        public int Scratched = 0;
+        public Cat(string name) : base(name)
+        {
+        }
+        public void Poke()
+        {
+            Console.WriteLine("You poked " + Name + ", he does not look happy! MEEEEOW! Watch your fingers!!");
+            Scratched++;
+            HappinessMeter =- 1;
+        }
+        public override void GiveBath()
+        {
+            Console.WriteLine(Name + " is not very happy to have a bath! HISSS ");
+            HappinessMeter =- 1;
+            TimesBathed++;
+        }
+        public void Results()
+        {
+            Console.WriteLine("Here are your Owner Results:");
+            Console.WriteLine("You were Scratched " + Scratched + " times!");
+            Console.WriteLine("You fed " + Name + " " + NumberOfMeals + " times!");
+            Console.WriteLine("You gave " + Name + " water " + TimesWatered + " times!");
+            Console.WriteLine("You gave " + Name + " " + TimesBathed + " baths!");
+            Console.WriteLine("Your pet " + Name + " had " + HappinessMeter + " happiness points!");
+        }
+    }
+    class Dog : Mammal
+    {
+        public int walksTaken;
+        public int bite;
+        public int tricksLearnt;
+        public Dog(string name) : base(name)
+        { 
+        }
+        public void Train()
+        {
+            Console.WriteLine("You took your time to teach " + Name + " a trick!");
+            tricksLearnt++;
+        }
+        public void Walk()
+        {
+            Console.WriteLine(Name + " is very happy on his walk! Such a good boy!");
+            HappinessMeter++;
+            walksTaken++;
+        }
+        public override void GiveFood()
+        {
+            Console.WriteLine(Name + " ate it's food super fast!! Careful you dont choke!");
+            NumberOfMeals++;
+        }
+        public override void GiveBath()
+        {
+            Console.WriteLine("What a mess! " + Name + " made such a big mess in the bath! Silly Puppy...");
+            HappinessMeter++;
+            TimesBathed++;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace Pet_Simulator
+{
+    class Program
+    {
+        public static void CatCareChoices(int pCatChoice, Cat cat)
+        {
+
+            if (pCatChoice == 1)
+            {
+                cat.GiveFood();
+            }
+            else if (pCatChoice == 2)
+            {
+                cat.PetHead();
+            }
+            else if (pCatChoice == 3)
+            {
+                cat.GiveWater();
+            }
+            else if (pCatChoice == 4)
+            {
+                cat.GiveBath();
+            }
+            else if (pCatChoice == 5)
+            {
+                cat.BrushFur();
+            }
+            else if (pCatChoice == 6)
+            {
+                cat.Poke();
+            }
+            else if (pCatChoice == 7)
+            {
+                Console.WriteLine("Thanks for playing!");
+            }
+        }
+        public static int UserChooseMenu(string pQuestion, string[] pOptions, out int pMenuChoice)
+        {
+            bool ValidInput = false; 
+            int input = 0;
+            do
+            {
+                Console.WriteLine(pQuestion);
+                Console.WriteLine("Please enter the number that represents your choice.");
+                for (int optionNum = 0; optionNum != pOptions.Length; optionNum++)
+                {
+                    Console.WriteLine((optionNum + 1) + ". " + pOptions[optionNum]);
+                }
+                try
+                {
+                    input = int.Parse(Console.ReadLine());
+                    ValidInput = true;
+                }
+                catch
+                {
+                    ValidInput = false;
+                }
+                pMenuChoice = input;
+                if (pMenuChoice > pOptions.Length + 1 || pMenuChoice <= 0)
+                {
+                    ValidInput = false;
+                    Console.WriteLine("That was invalid input! Please try again.");
+                }
+            } while (ValidInput == false);
+            return pMenuChoice;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the virtual pet simulation!");
+            //User Pet Adoption and Choice
+            string StartMenuQuestion = "What kind of pet would you like to adopt?";
+            string[] StartMenuOptions = { "Cat", "Dog", "Frog", "Snake", "Exit" };
+            UserChooseMenu(StartMenuQuestion, StartMenuOptions, out int MenuChoice);
+            Console.WriteLine("Please choose a name for your new " + (StartMenuOptions[MenuChoice - 1]));
+            string PetName = Console.ReadLine();
+            bool KeepPlaying = true;
+            //User Choices Acted Upon
+
+
+            //Cat Choice Options
+            if (MenuChoice == 1)
+            {
+                Cat MyFirstPet = new Cat(PetName);
+                do
+                {
+                    string CatQuestion = "What would you like to do with " + MyFirstPet.Name + " today?";
+                    string[] CatMenuOptions = { "Give Food", "Pet Head", "Give Water", "Give Bath", "Brush Fur", "Poke", "Exit" };
+                    UserChooseMenu(CatQuestion, CatMenuOptions, out int CatChoice);
+                    CatCareChoices(CatChoice, MyFirstPet);
+                    if (CatChoice == 7)
+                    {
+                        MyFirstPet.Results();
+                        KeepPlaying = false;
+                    }
+                } while (KeepPlaying == true);
+            }
+        }
+    }
+}
